7ad6e640d197a290bdcf1b082187e2bfd28ecba0
Davis Burrill <davisburrill@icloud.com>
2024-01-29 23:05:11 -0500

    updating week 3 again

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-# from .models import related models
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
-# Register your models here.
+@admin.register(CarMake)
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
 
-# CarModelInline class
-
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+@admin.register(CarModel)
+class CarModelAdmin(admin.ModelAdmin):
+    pass
